dafed68fe35d4dc0a9f4ecbdbdddeb150d86bd07
Simon Danielsson <116196524+simon-danielsson@users.noreply.github.com>
2026-05-04 00:02:09 +0200

    init

---
+++ src/main.c
@@ -0,0 +1,9 @@
+//! main entry point of program
+#define ANALIB_IMPLEMENTATION
+#include "env.h"
+
+// TODO: write a program
+int main(void) {
+    printf("Hello, %s!", ENV_AUTHOR);
+    return 0;
+}
