85667f43907a0f687d2c3969fed08289b53190c7
Simon Danielsson <116196524+simon-danielsson@users.noreply.github.com>
2026-05-09 21:57:58 +0200

    feat: conway optimizations and extra details

--- examples/conway.c
+++ examples/conway.c
@@ -1,6 +1,9 @@
 //! example conway's game of life
 #ifdef CONWAY
 
+#define GENERATIONS 100
+#define CYCLE_SLP_SEC 0.4
+
 #include <stdio.h>
 #include <time.h>
 
@@ -33,34 +36,67 @@ int main() {
     setup();
     GameOfLife gol;
     GameOfLife_init(&gol);
+
+    // init vars
     int generations;
+    int dead_cells;
+    int live_cells;
+    char buff[32];
+    char *clear = "           ";
+
     while (!gol.should_quit) {
         GameOfLife_init(&gol);
         generations = 0;
-        while (generations < 100 && !gol.should_quit) {
-            controls(&gol);
+        while (generations < GENERATIONS && !gol.should_quit) {
             // update
-            update_cells(&gol);
-            // draw
-            for (unsigned int i = 0; i < gol.cell_count; i++) {
-                tlk_cursor_mv(&gol.cells[i].p);
-                if (!gol.cells[i].alive &&
-                        gol.cells[i].alive != gol.cells[i].prev_state) {
-                    tlk_draw(".", tlk_style_default());
-                    continue;
-                }
-                if (gol.cells[i].alive &&
-                        gol.cells[i].alive != gol.cells[i].prev_state) {
-                    tlk_draw("█", tlk_style_default());
-                }
+            {
+                dead_cells = 0;
+                live_cells = 0;
+                controls(&gol);
+                update_cells(&gol);
             }
 
-            char buff[100];
-            snprintf(buff, sizeof(buff), "Generation: %d", generations);
-            tlk_cursor_move_home();
-            tlk_draw(buff, tlk_style_default());
-
+            // draw
+            {
+                // cells
+                for (unsigned int i = 0; i < gol.cell_count; i++) {
+                    tlk_cursor_mv(&gol.cells[i].p);
+                    if (!gol.cells[i].alive) {
+                        dead_cells++;
+                    } else {
+                        live_cells++;
+                    }
+                    if (!gol.cells[i].alive &&
+                            gol.cells[i].alive != gol.cells[i].prev_state) {
+                        tlk_draw("•", (Style){.bg = DEFAULT, .fg = RED});
+                        continue;
+                    }
+                    if (gol.cells[i].alive &&
+                            gol.cells[i].alive != gol.cells[i].prev_state) {
+                        tlk_draw("█", (Style){.bg = DEFAULT, .fg = BLUE});
+                    }
+                }
+                // gen
+                snprintf(buff, sizeof(buff), "Gen.: %d ", generations);
+                tlk_cursor_mv(&(Pos){.c = 0, .r = 1});
+                tlk_draw(clear, (Style){.bg = RED, .fg = BLACK});
+                tlk_cursor_mv(&(Pos){.c = 0, .r = 1});
+                tlk_draw(buff, (Style){.bg = RED, .fg = BLACK, .bold = true});
+                // live
+                snprintf(buff, sizeof(buff), "Live: %d ", live_cells);
+                tlk_cursor_mv(&(Pos){.c = 0, .r = 2});
+                tlk_draw(clear, (Style){.bg = RED, .fg = BLACK});
+                tlk_cursor_mv(&(Pos){.c = 0, .r = 2});
+                tlk_draw(buff, (Style){.bg = RED, .fg = BLACK, .bold = true});
+                // dead
+                snprintf(buff, sizeof(buff), "Dead: %d ", dead_cells);
+                tlk_cursor_mv(&(Pos){.c = 0, .r = 3});
+                tlk_draw(clear, (Style){.bg = RED, .fg = BLACK});
+                tlk_cursor_mv(&(Pos){.c = 0, .r = 3});
+                tlk_draw(buff, (Style){.bg = RED, .fg = BLACK, .bold = true});
+            }
             // cycle
+            al_sleepf(CYCLE_SLP_SEC);
             tlk_flush();
             generations++;
         }
@@ -97,10 +133,8 @@ void update_cells(GameOfLife *gol) {
                 if (i == 0 && j == 0) {
                     continue;
                 }
-
                 neighbor =
                     (Pos){.r = gol->cells[k].p.r + i, .c = gol->cells[k].p.c + j};
-
                 for (unsigned int p = 0; p < gol->cell_count; p++) {
                     if (gol->cells[p].p.r == neighbor.r &&
                             gol->cells[p].p.c == neighbor.c && gol->cells[p].alive) {
@@ -109,7 +143,6 @@ void update_cells(GameOfLife *gol) {
                 }
             }
         }
-
         if (gol->cells[k].alive) {
             next_alive[k] = live_neighbors == 2 || live_neighbors == 3;
         } else {
